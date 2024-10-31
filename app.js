620e874a731e10beac0f913bb23dec0fe51c2bd3
diba3 <drc9474@nyu.edu>
2024-10-31 09:22:18 -0400

    commit message

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const indexRoute = require("./routes/index.js");
+const aboutRoute = require("./routes/about.js");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", indexRoute);
+app.use("/about", aboutRoute);
 
 app.use(express.static("public"));
+
 app.listen(port, () => {
   console.log(`Example app listening pn port ${port}`);
 });
